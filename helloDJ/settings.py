dc3bc7ca7148df17dd9dd1d36682ae55170ead6d
yh <610685602@qq.com>
2017-05-10 17:10:53 +0800

    first html

--- helloDJ/settings.py
+++ helloDJ/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'ap-z!bv7dpl0oe%p3@93s*@#c^6osr3=6ukb_2c6_tsb(l)lzz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['coderyh.pythonanywhere.com']
+ALLOWED_HOSTS = ['coderyh.pythonanywhere.com',
+                 '127.0.0.1']
 
 
 # Application definition
